4a846837d1f0d9cd3af0bdfe41dcfc46b5ee87f7
redjax <jackenyon@gmail.com>
2023-04-11 23:27:45 +0000

    JSON DB:
    Update app to interact with local JSON file (mock DB).
    
    Add controller functions to UserController, handle CRUD operations on JSON db file
    
    Update constant, use dev_db_seed
    
    Add startup function to main() to demonstrate loading DB on startup
    
    Add validation to User Pydantic model

--- litestar_attrs_test/controllers/user_controller.py
+++ litestar_attrs_test/controllers/user_controller.py
@@ -18,7 +18,7 @@ from litestar.partial import Partial
 # from models.users_msgspec import User
 from models.users_pydantic import User
 
-from lib.db import load_db
+from lib.db import load_db, update_db
 
 
 class UserController(Controller):
@@ -28,11 +28,36 @@ class UserController(Controller):
     async def create_user(self, data: User) -> dict[str, Any]:
         print(f"Creating user from: {data}")
 
-        # new_user = {data.id: {"id": data.id, "name": data.name}}
-        # dev_user_db_init.update(new_user)
-        # print(f"Updated dev_user_db_init dict: {dev_user_db_init}")
+        try:
+            _db = load_db()
+            # print(f"Loaded DB: Type: {type(_db)}, contents:\n{_db}")
 
-        # return {"success": f"Created user {data.name} with ID {data.id}"}
+        except Exception as exc:
+            raise Exception(f"Error loading database. Exception details:\n{exc}")
+
+        users_keys = _db.keys()
+        users_values = _db.values()
+
+        if str(data.id) in users_keys:
+            print(f"[DEBUG] Found ID: '{data.id}' in DB keys. Skipping user creation")
+
+            return {
+                "skipped": f"User with ID '{data.id}' already exists. Skipping creation."
+            }
+
+        else:
+            # print(f"[DEBUG] User not found with ID '{data.id}'. Continuing.")
+            new_user = {data.id: {"id": data.id, "name": data.name}}
+            # print(f"[DEBUG] New user: {new_user}")
+
+            _db.update(new_user)
+            # print(f"Updated DB: {_db}")
+
+            _new_user_res = update_db(new_db=_db)
+
+            # print(f"New user creation results: {_new_user_res}")
+
+            return _new_user_res
 
     @get("/{user_id:int}")
     def retrieve_user(self, user_id: int) -> dict:
@@ -40,33 +65,124 @@ class UserController(Controller):
         Lookup user by ID. Return a User() object
         """
 
-        print(f"Retrieving user with ID: {user_id}")
+        try:
+            # print(f"[DEBUG] Retrieving user with ID: {user_id}")
+            _db = load_db()
+            # print(f"Loaded DB: Type: {type(_db)}, contents:\n{_db}")
 
-        _db = load_db()
-        print(f"Database: {_db}")
+        except Exception as exc:
+            raise Exception(f"Error loading database. Exception details:\n{exc}")
 
-        # _user = dev_user_db_init[user_id]
-        # print(f"Get User ({type(_user)}): {_user}")
+        users_keys = _db.keys()
+        users_values = _db.values()
 
-        # try:
-        #     user = User(**_user)
+        if str(user_id) in users_keys:
+            print(f"[DEBUG] Found ID: '{user_id}' in DB keys.")
 
-        # except Exception as exc:
-        #     raise Exception(f"[Exception] Error retrieving User object:\n{exc}")
+            try:
+                for _user in users_values:
+                    print(f"[DEBUG] User: {_user}")
 
-        # return user
+                    if _user["id"] == user_id:
+                        # print(f"[DEBUG] Matched user_id '{user_id}' to DB User: {_user['id']}")
+
+                        user = User.parse_obj(_user)
+                        print(f"[DEBUG] Parsed User: {user}")
+
+                        return user
+
+            except Exception as exc:
+                raise Exception(
+                    f"[ERROR] Error retrieving user with ID [{user_id}] from database. Exception details: {exc}"
+                )
+
+        else:
+            return {"error": f"User with ID '{user_id}' not found in database."}
 
     @patch(path="/{user_id:int}")
     async def update_user(self, user_id: int, data: Partial[User]) -> User:
         print(f"Partial update: ID: {user_id} - {data}")
 
-        # _update_user = {user_id: {"id": user_id, "name": data.name}}
-        # dev_user_db_init.update(_update_user)
+        try:
+            # print(f"[DEBUG] Retrieving user with ID: {user_id}")
+            _db = load_db()
+            # print(f"Loaded DB: Type: {type(_db)}, contents:\n{_db}")
 
-        # print(f"Updated dict: {dev_user_db_init}")
+        except Exception as exc:
+            raise Exception(f"Error loading database. Exception details:\n{exc}")
 
-        # return {"success": f"Updated user with ID '{user_id}': {data}"}
+        users_keys = _db.keys()
+        users_values = _db.values()
+
+        if str(user_id) in users_keys:
+            print(f"[DEBUG] [DEBUG] Found ID: '{user_id}' in DB keys.")
+
+            try:
+                for _user in users_values:
+                    # print(f"[DEBUG] User: {_user}")
+
+                    if _user["id"] == user_id:
+                        print(
+                            f"[DEBUG] Matched user_id '{user_id}' to DB User: ({type(_user['id'])}) {_user}"
+                        )
+                        update_user = {user_id: data.dict()}
+                        print(f"[DEBUG] New user: {update_user}")
+
+                        user = User.parse_obj(update_user[user_id])
+                        print(f"[DEBUG] Parsed User: {user}")
+
+                        _db[str(user_id)] = update_user[user_id]
+                        print(f"[DEBUG] Updated DB: {_db}")
+
+                        try:
+                            _update_res = update_db(new_db=_db)
+
+                            return _update_res
+
+                        except Exception as exc:
+                            return {
+                                "error": f"Error performing partial update on User with ID '{user_id}'. Exception details:\n{exc}"
+                            }
+
+            except Exception as exc:
+                raise Exception(
+                    f"[ERROR] Error retrieving user with ID [{user_id}] from database. Exception details: {exc}"
+                )
+
+        else:
+            return {"error": f"User with ID '{user_id}' not found in database."}
 
     @delete(path="/{user_id:int}")
     async def delete_user(self, user_id: int) -> None:
         print(f"Deleting user with ID: {user_id}")
+
+        try:
+            # print(f"[DEBUG] Retrieving user with ID: {user_id}")
+            _db = load_db()
+            # print(f"Loaded DB: Type: {type(_db)}, contents:\n{_db}")
+
+        except Exception as exc:
+            raise Exception(f"Error loading database. Exception details:\n{exc}")
+
+        users_keys = _db.keys()
+        users_values = _db.values()
+
+        if str(user_id) in users_keys:
+            print(f"[DEBUG] [DEBUG] Found ID: '{user_id}' in DB keys.")
+
+            try:
+                _db.pop(str(user_id))
+                print(f"Deleted User with ID '{user_id}'. New database: {_db}")
+
+                _delete_user_res = update_db(new_db=_db)
+                print(f"[DEBUG] Delete user results: {_delete_user_res}")
+
+                return _delete_user_res
+
+            except Exception as exc:
+                raise Exception(
+                    f"Error trying to delete User with ID '{user_id}'. Exception details:\n{exc}"
+                )
+
+        else:
+            return {"error": f"User with ID '{user_id}' not found in database."}
--- litestar_attrs_test/lib/db.py
+++ litestar_attrs_test/lib/db.py
@@ -1,7 +1,7 @@
 from __future__ import annotations
 from typing import TYPE_CHECKING, Dict, Union, Optional, Any
 
-from lib.constants import dev_user_db_dir, dev_user_db
+from lib.constants import dev_user_db_dir, dev_user_db, dev_db_seed
 
 if TYPE_CHECKING:
     ...
@@ -15,30 +15,69 @@ if not Path(dev_user_db_dir).exists():
     print(f"DB directory does not exist: [{dev_user_db_dir}]. Creating.")
     Path(dev_user_db_dir).mkdir(parents=True, exist_ok=True)
 
-db_seed = {0: {"id": 0, "name": "John Doe"}, 1: {"id": 1, "name": "Jane Doe"}}
+db_seed = dev_db_seed
 
 
 def create_init_db(
     db_path: str = db_path,
     seed_data: dict[int, dict[str, Union[str, int, None]]] = db_seed,
-):
+) -> None:
     if not Path(db_path).exists():
         print(f"DB does not exist: {db_path}")
         with open(db_path, "a+") as f_db:
             print(f"Working on DB file: {db_path}")
-            _dump = json.load(seed_data)
-            f_db.write(_dump)
+            json.dump(seed_data, f_db, indent=4)
+
+        f_db.close()
 
 
-def load_db(db_path: str = db_path) -> json.Dict:
+def load_db(db_path: str = db_path) -> dict[str, Any]:
     print(f"Attempting to load DB: {db_path}")
-    if Path(db_path).exists():
-        print(f"[DEBUG] DB exists: {db_path}")
-        with open(db_path, "r") as f_db:
-            db_dict = json.loads(f_db.read())
 
-            print(f"DB dict ({type(db_dict)}): {db_dict}")
+    _continue = True
+
+    while _continue:
+        if Path(db_path).exists():
+            _continue = False
+
+            print(f"[DEBUG] DB exists: {db_path}")
+            with open(db_path, "r") as f_db:
+                db_dict = json.loads(f_db.read())
+                # print(f"DB dict ({type(db_dict)}): {db_dict}")
+
+            f_db.close()
+
+            return db_dict
+
+        else:
+            print(f"[DEBUG] DB does not exist: {db_path}")
+            create_init_db(db_path=db_path)
+
+            load_db(db_path=db_path)
+
+
+def update_db(
+    db_path: str = db_path, new_db: dict[Union[int, str], Union[Any, None]] = None
+):
+    print(f"[DEBUG] Overwriting DB data with: {new_db}")
+    print(f"Attempting to load DB: {db_path}")
+
+    _continue = True
+
+    if Path(db_path).exists():
+        _continue = False
+
+        print(f"[DEBUG] DB exists: {db_path}")
+
+        with open(db_path, "w+") as f_db:
+            json.dump(new_db, f_db, indent=4)
+
+        f_db.close()
+
+        return {"success": f"Database updated successfully."}
 
     else:
         print(f"[DEBUG] DB does not exist: {db_path}")
         create_init_db(db_path=db_path)
+
+        update_db(db_path=db_path, new_db=new_db)
